08802ceebb9ce6b866d9982e8b7d390f4ad0645b
SneakyBuzZ <kaushikx304@gmail.com>
2025-07-05 22:28:30 +0530

    resolve build error

--- apps/web/lib/hooks/use-chat-completion.tsx
+++ apps/web/lib/hooks/use-chat-completion.tsx
@@ -1,6 +1,5 @@
 import { useCompletion } from "@ai-sdk/react";
 import useFileStore from "../store/file-store";
-import { delay } from "@chatgpt/utils";
 
 export function useChatCompletion(chatSessionId: string) {
   const { reset } = useFileStore();
@@ -8,7 +7,6 @@ export function useChatCompletion(chatSessionId: string) {
     api: `/api/message/${chatSessionId}`,
     onFinish: () => {
       reset();
-      delay(3000).then(() => console.log("Upload reset"));
     },
   });
 
